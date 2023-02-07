83df7d50b99e773f487fb515c56adacb86fc2a40
Miro Airas <apeai001@edu.xamk.fi>
2023-02-07 10:48:09 +0200

    Add project files.

---
+++ Auto/Ajoneuvo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Auto
+{
+    internal class Ajoneuvo
+    {
+        public Ajoneuvo(int vuosimalli, string merkki, string malli, int nopeus)
+        {
+            Vuosimalli = vuosimalli;
+            Merkki = merkki;
+            Malli = malli;
+            Nopeus = nopeus;
+        }
+
+        public int Vuosimalli { get; set; }
+        public string Merkki { get; set; }
+        public string Malli  { get; set; }
+        public int Nopeus{ get; set; }
+    }
+   public void 
+}
---
+++ Auto/Program.cs
@@ -0,0 +1,68 @@
+﻿using System.Security.AccessControl;
+
+int nopeus=0;
+int luku;
+char kirjain;
+bool jatkuu;
+
+Console.WriteLine("Tästä lähtee");
+do
+{
+
+
+    
+    Console.WriteLine("lisätäänkö vai lasketaanko nopeutta?");
+    Console.WriteLine("a/b");
+    kirjain=Convert.ToChar(Console.ReadLine());  
+    switch (kirjain)
+    {
+        case 'a':
+          nopeus=
+            nopeus+5;
+         break;
+
+        case 'b':
+          nopeus=
+            nopeus-5; 
+         break;
+
+        default:
+         nopeus = 0; break;
+        
+       
+    }
+    do
+    {
+        if (nopeus>=26)
+        
+        {
+            Console.WriteLine("et saa mennä kovempaa");
+            nopeus = 25;
+            break;
+        }
+    } while (nopeus>=26);
+    do
+    {
+        if (nopeus <=-1)
+
+        {
+            Console.WriteLine("et saa mennä hiljempaa kuin 0 KM/h");
+            nopeus = 0;
+            break;
+        }
+    } while (nopeus <=-1);
+
+    Console.WriteLine("nopeutesi on {0} KM/h",nopeus);
+    Console.WriteLine("Jatketaanko? k/e");
+    string valinta=Console.ReadLine();
+    if (valinta.Contains("k")) 
+    {
+        jatkuu= true;
+    }
+    else
+    {
+        Console.WriteLine("Auto on pysähtynyt.");
+        jatkuu= false;
+    }
+
+} while (jatkuu) ;
